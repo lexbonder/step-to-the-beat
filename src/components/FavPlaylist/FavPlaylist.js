a6f9ced29e73c292ba1c86b92d43b85dd1dd4c31
Alex Bonder <lexbonder@gmail.com>
2018-02-19 17:38:45 -0700

    add styles to favorite playlist screen and reduce size of text

--- src/components/FavPlaylist/FavPlaylist.js
+++ src/components/FavPlaylist/FavPlaylist.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './FavPlaylist.css';
 
 export const FavPlaylist = () => {
   return (
-    <div>
+    <div className='favPlaylist'>
       <h2>Playlist Name</h2>
       <h2>SPM</h2>
       <h2>Speed/Pace</h2>
