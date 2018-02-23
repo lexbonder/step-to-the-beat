0854286bbfd5f6e37b1d910fd08da7f2795dd029
Alex Bonder <lexbonder@gmail.com>
2018-02-23 16:31:53 -0700

    Prepare app for heroku

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,8 +10,7 @@ export const getUserName = async (accessToken) => {
 };
 
 export const getPlaylistData = async (bpm, genre, accessToken) => {
-  const initialFetch = await fetch(`https://api.spotify.com/v1/recommendations
-    ?seed_genres=${genre}&target_tempo=${bpm}`,
+  const initialFetch = await fetch(`https://api.spotify.com/v1/recommendations?seed_genres=${genre}&target_tempo=${bpm}`,
       { headers: { Authorization: 'Bearer ' + accessToken }
       });
   return await initialFetch.json();
@@ -21,8 +20,7 @@ export const createNewPlaylist = async (userId, accessToken, playlistName) => {
   const playlistBody = {
     name: `${playlistName}, made with Step to the Beat`
   };
-  const initialFetch = await fetch(`https://api.spotify.com/v1/users
-    /${userId}/playlists`, {
+  const initialFetch = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
     method: 'POST',
     body: JSON.stringify(playlistBody),
     headers: {
@@ -39,8 +37,7 @@ export const createNewPlaylist = async (userId, accessToken, playlistName) => {
 
 export const populatePlaylist = async (userId, playlistId, accessToken, tracks) => {
   const playlistTracks = {uris: tracks};
-  const initialFetch = await fetch(`https://api.spotify.com/v1/users
-    /${userId}/playlists/${playlistId}/tracks`, {
+  const initialFetch = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
     method: 'POST',
     body: JSON.stringify(playlistTracks),
     headers: {
