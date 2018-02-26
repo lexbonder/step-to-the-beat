184faaa18055bcbebacba8f648804be2d6e3efed
Alex Bonder <lexbonder@gmail.com>
2018-02-26 09:55:38 -0700

    Fix LoadingGif test to override random closes #67

--- src/components/LoadingGif/LoadingGif.test.js
+++ src/components/LoadingGif/LoadingGif.test.js
@@ -4,6 +4,7 @@ import LoadingGif from './LoadingGif';
 
 describe('LoadingGif', () => {
   it('should match the snapshot', () => {
+    Math.random = jest.fn().mockImplementation(() => 0)
     const wrapper = shallow(<LoadingGif />);
     expect(wrapper).toMatchSnapshot();
   });
